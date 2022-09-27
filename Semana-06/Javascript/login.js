acf2343c19aaa2ea8a394b99c98c5a1c5a382f95
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-09-27 02:38:59 -0300

    BaSP-W06-2022: Validations in login.js done

--- Semana-06/Javascript/login.js
+++ Semana-06/Javascript/login.js
@@ -10,7 +10,7 @@ userEmail.onblur = function (){
     }
     else{
         userEmail.classList.add('red-border');
-        errorAlert.innerHTML = 'Error: debe ingresar un E-Mail valido';
+        errorAlert.innerHTML = 'Error: you must enter a valid E-Mail';
         userEmail.parentNode.insertBefore(errorAlert, userEmail.nextElementSibling);
     }
 }
@@ -19,5 +19,53 @@ userEmail.onfocus = function (){
 }
     //PasswordValidation
 var password = document.getElementById('password');
-
+function passwordError() {
+    password.classList.add('red-border');
+    errorAlert.innerHTML = 'Error: you must enter a password';
+    password.parentNode.insertBefore(errorAlert, userEmail.nextElementSibling);
+} 
+numsAndLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+function PasswordValidation(pass,type){
+    var passLength = pass.length;
+    for (i = 0; i < passLength; i++){
+        caracters = pass[i];
+        if (numsAndLetters.indexOf(caracters) ==-1){
+            return (false);
+        }
+    return (true);
+    }
+}
+password.onblur = function (){
+    if (password.value == ''){
+        passwordError();
+    }
+    else if (!PasswordValidation(password.value, numsAndLetters)){
+        passwordError();
+    }
+    else{
+        password.classList.add('green-border');
+    }
+}
+password.onfocus = function (){
+    password.classList.remove('red-border');
+}
+    //Button
+var buttonLogin = document.getElementsByClassName('button-Continue')[0];
+console.log(buttonLogin)
+buttonLogin.onclick = function (e){
+    e.preventDefault();
+    email = userEmail.value;
+    passwordValue = password.value;
+    if (PasswordValidation(passwordValue, numsAndLetters) && emailExpression.test (email)){
+        alert('Username: ' + email + ' ' + 'Password: ' + passwordValue);
+    }
+    else {
+        if (!emailExpression.test (email)){
+            alert("Invalid Username");
+        }
+        if (!PasswordValidation(passwordValue, numsAndLetters)){
+            alert("Invalid Password");
+        }
+    }
+}
 }
